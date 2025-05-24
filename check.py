40ad8bb500b5d0fb8f19e3b0d4d8521d898f9085
xronak007 <amiragupta004@gmail.com>
2025-05-24 17:28:38 +0530

    Add files via upload

--- check.py
+++ check.py
@@ -408,5 +408,7 @@ def cvv_check():
         return f"404 Payment declined\n\nResponse:\n{result2}"    
         
 #Complete
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
